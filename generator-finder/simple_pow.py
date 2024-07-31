9bea51180587cedd11862360a2463ecade7770d0
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-07-31 18:36:22 +0900

    ✨ Feat : make reusable

--- generator-finder/simple_pow.py
+++ generator-finder/simple_pow.py
@@ -1,48 +1,54 @@
-def solve_1():
-    # calculate 3233^9517 ≡ x (mod 11413)
-    # denote 9517 = 8,192 + 1024 + 256 + 32  +  8  +  4  +  1
-    #             = 2^13  + 2^10 + 2^8 + 2^5 + 2^3 + 2^2 + 2^0
+def _base_2_exp(n: int):
+    return [i for i in range(0, n.bit_length()) if n & (1 << i)]
 
+
+def solve_1(_base: int, _exp: int, _mod: int) -> int:
     res = 1
-    for i in [13, 10, 8, 5, 3, 2, 0]:
-        res *= pow(3233, 2 ** i, 11413)
-        res %= 11413
+    for i in _base_2_exp(_exp)[::-1]:
+        res *= pow(_base, 2 ** i, _mod)
+        res %= _mod
         print(f"{i:2}: {res}")
 
-    print("-" * 10)
+    print("-" * 15)
     print(f"solve_1: {res}")
+    return res
 
 
-def solve_2():
-    # calculate 3233^9517 ≡ x (mod 11413)
-    # start from 3233^2 ^ 11413
-    # no update of res until exp is odd
+def solve_2(_base: int, _exp: int, _mod: int) -> int:
     res = 1
-    _base = 3233
-    _exp = 9517
-    _mod = 11413
 
-    def get_new_base_exp(base, exp, mod):
-        if base == 1:
-            return base, 0
-        if exp % 2 == 1:
-            nonlocal res
-            exp -= 1
-            res = res * base % mod
-            print(f"{res:5} | {base:5}^{exp + 1:5} ≡ {base:5}^{exp:5} * {base} (mod {mod})")
-        new_base = base ** 2 % mod
-        new_exp = exp // 2
-        print(f"{res:5} | {base:5}^{exp:5} ≡ {new_base:5}^{new_exp:5} \t\t (mod {mod})")
+    def get_new_base_exp(_base, _exp, _mod):
+        nonlocal res
+        if _base == 1:
+            return _base, 0
+        if _exp % 2 == 1:
+            _exp -= 1
+            res = res * _base % _mod
+            print(f"{res:5} | {_base:5}^{_exp + 1:5} ≡ {_base:5}^{_exp:5} * {_base} (mod {_mod})")
+        new_base = _base ** 2 % _mod
+        new_exp = _exp // 2
+        print(f"{res:5} | {_base:5}^{_exp:5} ≡ {new_base:5}^{new_exp:5} \t\t (mod {_mod})")
         return new_base, new_exp
 
     while _exp > 0:
         _base, _exp = get_new_base_exp(_base, _exp, _mod)
 
-    print("-" * 10)
+    print("-" * 15)
     print(f"solve_2: {res}")
+    return res
 
 
 if __name__ == '__main__':
-    solve_1()
+    base, exp, mod = 3233, 9517, 11413
+
+    # calculate 3233^9517 ≡ x (mod 11413)
+    # denote 9517 = 8,192 + 1024 + 256 + 32  +  8  +  4  +  1
+    #             = 2^13  + 2^10 + 2^8 + 2^5 + 2^3 + 2^2 + 2^0
+    a1 = solve_1(base, exp, mod)
     print("")
-    solve_2()
+
+    # calculate 3233^9517 ≡ x (mod 11413)
+    # start from 3233^2 ^ 11413
+    # no update of res until exp is odd
+    a2 = solve_2(base, exp, mod)
+    assert a1 == a2
