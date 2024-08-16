d57edc0fff11f970fa958bcab99bdd9801e58924
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-08-16 17:24:36 +0900

    🛠️ Chore : init questions

---
+++ crypto-batch/Q2.py
@@ -0,0 +1,82 @@
+import hashlib
+import random
+
+from Crypto.Util.number import getStrongPrime, bytes_to_long
+
+p = getStrongPrime(512)
+g = 2
+x = random.randint(2, p - 2)
+y = pow(g, -x, p)
+
+print(f"{p = }")
+print(f"{y = }")
+
+
+def sign(m, x, p):
+    k = random.randint(2, p - 2)
+    r = pow(g, k, p)
+
+    e = bytes_to_long(hashlib.sha256(str(r).encode() + m.encode()).digest())
+    s = (k + x * e) % (p - 1)
+    return r, s
+
+
+def verify(sig, m, y, p):
+    r, s = sig
+    e = bytes_to_long(hashlib.sha256(str(r).encode() + m.encode()).digest())
+
+    return (pow(g, s, p) * pow(y, e, p) % p) == r
+
+
+sig1 = sign('upside', x, p)
+assert verify(sig1, 'upside', y, p)
+
+sig2 = sign('academy', x, p)
+assert verify(sig2, 'academy', y, p)
+
+sig3 = sign('best', x, p)
+assert verify(sig3, 'best', y, p)
+
+print(f"{sig1 = }")
+print(f"{sig2 = }")
+print(f"{sig3 = }")
+
+
+def batch_verify(sigs, ms, ts, y, p):
+    es = [bytes_to_long(hashlib.sha256(str(ri).encode() + mi.encode()).digest()) for (ri, _), mi in zip(sigs, ms)]
+
+    l, r = 1, 1
+    for i in range(len(sigs)):
+        l *= pow(g, ts[i] * sigs[i][1], p)
+        l *= pow(y, es[i] * ts[i], p)
+        l %= p
+
+        r *= pow(sigs[i][0], ts[i], p)
+        r %= p
+
+    return l == r
+
+
+assert batch_verify([sig1, sig2, sig3], ['upside', 'academy', 'best'], [3, 5, 7], y, p)
+
+coefs = [random.randint(2, p - 2) for _ in range(6)]
+print(f"Coefs are: {coefs}")
+
+print("Give me your signatures!")
+r1 = int(input('r1 > ')) % p
+s1 = int(input('s1 > ')) % (p - 1)
+r2 = int(input('r2 > ')) % p
+s2 = int(input('s2 > ')) % (p - 1)
+r3 = int(input('r3 > ')) % p
+s3 = int(input('s3 > ')) % (p - 1)
+
+if any((ri, si) in [sig1, sig2, sig3] for ri, si in [(r1, s1), (r2, s2), (r3, s3)]):
+    print("NO HACK")
+    exit(0)
+
+assert batch_verify([(r1, s1), (r2, s2), (r3, s3)], ['upside', 'academy', 'best'], coefs[:3], y, p)
+assert batch_verify([(r1, s1), (r2, s2), (r3, s3)], ['upside', 'academy', 'best'], coefs[3:], y, p)
+
+with open('./flag', 'r') as f:
+    flag = f.read()
+    print("Here is the flag:", flag)
---
+++ crypto-batch/Q3.py
@@ -0,0 +1,73 @@
+import hashlib
+import random
+
+from Crypto.Util.number import *
+
+while True:
+    p = getStrongPrime(512)
+    if p % 4 == 3:
+        break
+
+
+def is_qr(v, p):
+    return pow(v, (p - 1) // 2, p) == 1
+
+
+def sqrt(v, p):
+    assert p % 4 == 3
+    assert is_qr(v, p)
+
+    return pow(v, (p + 1) // 4, p)
+
+
+while True:
+    d = random.randint(2, p - 1)
+    if not is_qr(d, p):
+        break
+
+
+# Equation is: -x^2 + y^2 = 1 + d*x^2*y^2
+
+def random_point(d, p):
+    while True:
+        x = random.randint(1, p - 1)
+        y_square = (-x ** 2 - 1) * pow(d * x ** 2 - 1, -1, p)
+
+        if is_qr(y_square, p):
+            y = sqrt(y_square, p)
+            return x, y
+
+
+def add(P, Q, d, p):
+    # NOTE: IMPLEMENT THIS
+    pass
+
+
+def double(P, d, p):
+    # NOTE: IMPLEMENT THIS
+    pass
+
+
+def is_on_curve(P, d, p):
+    x, y = P
+    return (-x * x + y * y) % p == (1 + d * x * x * y * y) % p
+
+
+P = random_point(d, p)
+Q = random_point(d, p)
+
+P2Q2 = add(double(P, d, p), double(Q, d, p), d, p)
+P2Q2_ = double(add(P, Q, d, p), d, p)
+
+assert P2Q2 == P2Q2_
+assert is_on_curve(P2Q2, d, p)
+
+print(f"{p = }")
+print(f"{d = }")
+
+print(f"{P = }")
+print(f"{Q = }")
+
+x, y = P2Q2
+flag = hashlib.sha256(long_to_bytes(x) + long_to_bytes(y)).hexdigest()
+print("Flag is DH{" + flag + "}")
